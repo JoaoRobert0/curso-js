a5c8c61506440543a6518ccec7511870e7dd0a40
JoaoRobert0 <devjoaoroberto@gmail.com>
2024-09-21 15:58:39 -0300

    Aula de estruturas condicionais

---
+++ secao_3/estruturas_condicionais/index.js
@@ -0,0 +1,15 @@
+/* 
+Estruturas Condicionais
+já sei desse conteudo então irei fazer um pequno codigo
+*/
+
+let numero = 6;
+
+if (numero >= 0 && numero <= 3) {
+    console.log("O numero está entre 0 ou 3");
+} else if (numero <= 5) {
+    console.log("O numero está entre 4 ou 5");
+} else {
+    console.log("O numero não está entre 0 ou 5");
+}
+
