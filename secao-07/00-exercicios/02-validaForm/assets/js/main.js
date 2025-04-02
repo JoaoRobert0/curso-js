e6dea9b2c4c7e50c336ce4a39d08ae8253032478
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-04-02 14:07:47 -0300

    validador de form com classes

--- secao-07/00-exercicios/02-validaForm/assets/js/main.js
+++ secao-07/00-exercicios/02-validaForm/assets/js/main.js
@@ -1,93 +1,99 @@
-const form = document.querySelector(".formulario")
-
-function validarForm() {
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
-        limparErros();
-
+class ValidaFormulario {
+    constructor() {
+        this.form = document.querySelector(".formulario")
+        this.validarForm()
+    }
+    
+    validarForm() {
+        this.form.addEventListener("submit", (event) => {
+            event.preventDefault()
+            this.limparErros();
+    
+            let erros = 0
+            for (const node of document.querySelectorAll(".validar")) {
+                erros += this.validarInput(node)
+            }
+    
+            if (erros == 0) {
+                alert("Formulario enviado")
+                this.form.submit()
+            } else {
+                alert("Corrija formulario")
+            }
+        })
+    }
+    
+    limparErros() {
+        document.querySelectorAll(".error-text").forEach(erro => erro.remove());
+    }
+    
+    isVazio(element) {
+        return element.value === ""
+    }
+    
+    criarErrorText(textContent) {
+        const p = document.createElement('p');
+        p.textContent = textContent;
+        p.classList.add("error-text");
+        return p
+    }
+    
+    senhasIguais() {
+        return document.querySelector(".senha").value === document.querySelector(".repetir-senha").value
+    }
+    
+    validarInput(inputElement) {
         let erros = 0
-        for (const node of document.querySelectorAll(".validar")) {
-            erros += validarInput(node)
+        const labelText = inputElement.previousElementSibling.textContent;
+    
+        if (this.isVazio(inputElement)) {
+            inputElement.parentNode.insertBefore(this.criarErrorText(`O campo ${labelText} está vazio.`), inputElement.nextSibling);
+            erros++;
         }
-
-        if (erros == 0) {
-            alert("Formulario enviado")
-            form.submit()
-        } else {
-            alert("Corrija formulario")
+    
+        if (inputElement.classList.contains("usuario")) {
+            if (inputElement.value.length < 3 || inputElement.value.length > 12 ) {
+                inputElement.parentNode.insertBefore(this.criarErrorText(`${labelText} precisa ter entre 3 e 12 caracteres.`), inputElement.nextSibling);
+                erros++;
+            }
+    
+            const regex = /^[a-zA-Z0-9]+$/
+            if (!regex.test(inputElement.value)) {
+                inputElement.parentNode.insertBefore(this.criarErrorText(`Nome de ${labelText} precisar conter apenas letras e/ou números.`), inputElement.nextSibling);
+                erros++;
+            }
         }
-    })
-}
-
-function limparErros() {
-    document.querySelectorAll(".error-text").forEach(erro => erro.remove());
-}
-
-function isVazio(element) {
-    return element.value === ""
-}
-
-function criarErrorText(textContent) {
-    const p = document.createElement('p');
-    p.textContent = textContent;
-    p.classList.add("error-text");
-    return p
-}
-
-function senhasIguais() {
-    return document.querySelector(".senha").value === document.querySelector(".repetir-senha").value
-}
-
-function validarInput(inputElement) {
-    let erros = 0
-    const labelText = inputElement.previousElementSibling.textContent;
-
-    if (isVazio(inputElement)) {
-        inputElement.parentNode.insertBefore(criarErrorText(`O campo ${labelText} está vazio.`), inputElement.nextSibling);
-        erros++;
+    
+        if (inputElement.classList.contains("cpf")) {
+            if (!CPF.validar(inputElement.value)) {
+                inputElement.parentNode.insertBefore(this.criarErrorText(`CPF inválido.`), inputElement.nextSibling);
+                erros++;
+            }
+        }
+    
+        if (inputElement.classList.contains("senha")) {
+            if (inputElement.value.length < 6 || inputElement.value.length > 12 ) {
+                inputElement.parentNode.insertBefore(this.criarErrorText(`${labelText} precisa ter entre 6 e 12 caracteres.`), inputElement.nextSibling);
+                erros++;
+            }
+    
+            if (!this.senhasIguais()) {
+                inputElement.parentNode.insertBefore(this.criarErrorText(`Campos senha e repetir senha precisar ser iguais.`), inputElement.nextSibling);
+                erros++;
+            }
+        }
+    
+        if (inputElement.classList.contains("repetir-senha")) {
+            if (!this.senhasIguais()) {
+                inputElement.parentNode.insertBefore(this.criarErrorText(`Campos senha e repetir senha precisar ser iguais.`), inputElement.nextSibling);
+                erros++;
+            }
+        }
+    
+        return erros
     }
-
-    if (inputElement.classList.contains("usuario")) {
-        if (inputElement.value.length < 3 || inputElement.value.length > 12 ) {
-            inputElement.parentNode.insertBefore(criarErrorText(`${labelText} precisa ter entre 3 e 12 caracteres.`), inputElement.nextSibling);
-            erros++;
-        }
-
-        const regex = /^[a-zA-Z0-9]+$/
-        if (!regex.test(inputElement.value)) {
-            inputElement.parentNode.insertBefore(criarErrorText(`Nome de ${labelText} precisar conter apenas letras e/ou números.`), inputElement.nextSibling);
-            erros++;
-        }
-    }
-
-    if (inputElement.classList.contains("cpf")) {
-        if (!CPF.validar(inputElement.value)) {
-            inputElement.parentNode.insertBefore(criarErrorText(`CPF inválido.`), inputElement.nextSibling);
-            erros++;
-        }
-    }
-
-    if (inputElement.classList.contains("senha")) {
-        if (inputElement.value.length < 6 || inputElement.value.length > 12 ) {
-            inputElement.parentNode.insertBefore(criarErrorText(`${labelText} precisa ter entre 6 e 12 caracteres.`), inputElement.nextSibling);
-            erros++;
-        }
-
-        if (!senhasIguais()) {
-            inputElement.parentNode.insertBefore(criarErrorText(`Campos senha e repetir senha precisar ser iguais.`), inputElement.nextSibling);
-            erros++;
-        }
-    }
-
-    if (inputElement.classList.contains("repetir-senha")) {
-        if (!senhasIguais()) {
-            inputElement.parentNode.insertBefore(criarErrorText(`Campos senha e repetir senha precisar ser iguais.`), inputElement.nextSibling);
-            erros++;
-        }
-    }
-
-    return erros
 }
+
 
 class CPF {
     static validar(cpf) {
@@ -139,4 +145,5 @@ class CPF {
     }
 }
 
-validarForm()
+const vf = new ValidaFormulario;
+console.log(vf);
